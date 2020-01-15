9afbf342362b9ae5f1f5c182a3f50e62f70fdeef
Antono (cremi) <antonin33290@gmail.com>
2020-01-15 12:38:34 +0100

    Debut 2.2

---
+++ TD1/mds3d_td1/src/direct.cpp
@@ -0,0 +1,21 @@
+#include "integrator.h"
+#include "scene.h"
+
+class DirectIntegrator : public Integrator {
+public:
+    DirectIntegrator(const PropertyList &props) {
+        /* No parameters this time */
+    }
+
+    Color3f Li(const Scene *scene, const Ray &ray) const {
+        /** TODO : Find the surface that is visible in the requested direction
+                   Return its ambiant color */
+ 
+    }
+
+    std::string toString() const {
+        return "DirectIntegrator[]";
+    }
+};
+
+REGISTER_CLASS(DirectIntegrator, "direct")
