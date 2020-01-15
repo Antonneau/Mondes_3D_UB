22982088333900bf11de53ab51f0982a94d0c7b5
Antono (cremi) <antonin33290@gmail.com>
2020-01-15 11:49:19 +0100

    FIN EXERCICE 1
    Affichage de sphères et de plans.

--- TD1/mds3d_td1/src/plane.cpp
+++ TD1/mds3d_td1/src/plane.cpp
@@ -16,9 +16,21 @@ Plane::~Plane()
 
 bool Plane::intersect(const Ray& ray, Hit& hit) const
 {
-    /// TODO
+    Point3f o = ray.origin;
+    Vector3f d = ray.direction;
+    Point3f cen = m_position;
+    Point3f n = m_normal;
 
-    return true;
+    float t = ((cen-o).dot(n))/(d.dot(n));
+
+    bool res = false;
+    if(t > 0){
+        hit.setT(t);
+        hit.setShape(this);
+        res= true;
+    }
+
+    return res;
 }
 
 REGISTER_CLASS(Plane, "plane")
