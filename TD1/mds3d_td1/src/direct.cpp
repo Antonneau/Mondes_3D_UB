78b6d1d3716714a96bd010d195dd61f73a420140
Antono (cremi) <antonin33290@gmail.com>
2020-01-20 11:46:43 +0100

    Maj fonction "Li"
    Lumière bien pris en compte.
    Certaines lumières ne sont cependant pas reflétées (exemple de la sphère bleue dans "troissphères.scn")

--- TD1/mds3d_td1/src/direct.cpp
+++ TD1/mds3d_td1/src/direct.cpp
@@ -17,8 +17,9 @@ public:
             color.setZero();
             Vector2f uv(hit.shape()->material()->textureScaleU(), hit.shape()->material()->textureScaleV());
             for(uint i = 0; i < scene->lightList().size(); i++){
+                cout << "Iterating a new light : " << i << endl;
                 Point3f inter(ray.origin + (ray.direction*hit.t()));
-                Color3f p = hit.shape()->material()->brdf(ray.direction, scene->lightList().at(i)->direction(inter), hit.normal(), uv);
+                Color3f p = hit.shape()->material()->brdf(ray.direction, (scene->lightList().at(i)->direction(inter))*(-1), hit.normal(), uv);
                 float theta = (scene->lightList().at(i)->direction(inter)).dot(hit.normal());
                 if (theta < 0){
                     theta = 0;
